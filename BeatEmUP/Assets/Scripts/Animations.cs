80154a3617541add7a4b596937ff4dea371daabc
dilligak2362 <dilligak2362@student.faytechcc.edu>
2023-04-09 11:20:46 -0500

    Animations for Slash is imported and on character

---
+++ BeatEmUP/Assets/Scripts/Animations.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animations : MonoBehaviour
+{
+    private Animator anim;
+    public float cooldownTime = 2f;
+    private float nextFireTime = 0f;
+    public static int noOfClicks = 0;
+    float lastClickedTime = 0;
+    float maxComboDelay = 1;
+    public bool animation_bool;
+
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.K))
+        {
+            anim.Play("Slash");
+        }
+    }
+}
+
